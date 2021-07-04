a500659dccc31cb01cb4bcd608ec38ba35793b54
RaminHels <r.finland88@gmail.com>
2021-07-04 19:08:57 +0430

    Changed namespace to Omega

--- App/Program.cs
+++ App/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace DotnetWeb
+namespace Omega
 {
     public class Program
     {

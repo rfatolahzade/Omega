022b0643335b7359bec026f6670870ca5799845f
RaminHels <r.finland88@gmail.com>
2021-07-04 19:11:36 +0430

    Changed namespace to App

--- App/Program.cs
+++ App/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace Omega
+namespace App
 {
     public class Program
     {
